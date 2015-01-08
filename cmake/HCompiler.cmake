79f323249223666eed0a1576bd3a86e4a3086d4f
Miquel Sabaté <mikisabate@gmail.com>
2015-01-08 13:07:18 +0100

    cmake: added -Werror into the mix, and moved away GCC-specific flags.

--- cmake/HCompiler.cmake
+++ cmake/HCompiler.cmake
@@ -5,6 +5,9 @@ if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
   if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.8)
     message(FATAL_ERROR "GCC version must be at least 4.8!")
   endif()
+
+  # Add special GCC flags.
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wlogical-op")
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   # Require at least Clang 3.5
   if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.4)
@@ -21,6 +24,5 @@ set(CMAKE_CXX_FLAGS_RELEASE        "-O3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
 
 # And now we set our generic C++ flags.
-# TODO: -Werror
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y -Wpedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wswitch-default -Wundef")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y -Werror -Wpedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Winit-self -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wswitch-default -Wundef")
 
